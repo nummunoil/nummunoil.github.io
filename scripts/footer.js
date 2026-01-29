b2847f53c81985f3dd0ae2c4e6032e55c12ec343
Oil <sudarat@1moby.com>
2026-01-29 23:22:32 +0700

    update info

---
+++ scripts/footer.js
@@ -0,0 +1,24 @@
+(function () {
+  window.FOOTER_HTML =
+    '<footer class="pt-4 pb-4 text-center bg-light">' +
+    '<div class="container">' +
+    '<div class="my-3">' +
+    '<div class="h4">Sudarat Worawetwichai</div>' +
+    '<p>Web Developer</p>' +
+    '<div class="social-nav">' +
+    '<nav role="navigation">' +
+    '<ul class="nav justify-content-center">' +
+    '<li class="nav-item"><a class="nav-link" href="https://github.com/nummunoil" title="GitHub"><i class="fab fa-github"></i><span class="menu-title sr-only">GitHub</span></a></li>' +
+    '<li class="nav-item"><a class="nav-link" href="https://www.facebook.com/nummunoil" title="Facebook"><i class="fab fa-facebook"></i><span class="menu-title sr-only">Facebook</span></a></li>' +
+    '<li class="nav-item"><a class="nav-link" href="https://www.instagram.com/nummunoils" title="Instagram"><i class="fab fa-instagram"></i><span class="menu-title sr-only">Instagram</span></a></li>' +
+    '</ul>' +
+    '</nav>' +
+    '</div>' +
+    '</div>' +
+    '<div class="text-small text-secondary">' +
+    '<div class="mb-1">&copy; Super Folio. All rights reserved.</div>' +
+    '<div>Design - <span>TemplateFlip</span></div>' +
+    '</div>' +
+    '</div>' +
+    '</footer>';
+})();
